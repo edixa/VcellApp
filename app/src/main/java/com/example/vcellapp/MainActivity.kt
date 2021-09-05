0d91bc38439a60065cc1816749fc207ca7eb89df
Ed <75823130+edixa@users.noreply.github.com>
2021-09-05 13:30:51 -0500

    cree una funcion llenar tabla, para actualizar y mostrar los datos

--- app/src/main/java/com/example/vcellapp/MainActivity.kt
+++ app/src/main/java/com/example/vcellapp/MainActivity.kt
@@ -3,8 +3,10 @@ package com.example.vcellapp
 import android.content.ContentValues
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.EditText
+import android.widget.TableLayout
 import android.widget.TextView
 import android.widget.Toast
 
@@ -14,6 +16,8 @@ class MainActivity : AppCompatActivity() {
     var edtModelo:EditText?=null
     var edtEntrada:EditText?=null
     var edtSalida:EditText?=null
+    var tlCelulares:TableLayout?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,6 +27,8 @@ class MainActivity : AppCompatActivity() {
         edtModelo=findViewById(R.id.edtModelo)
         edtEntrada=findViewById(R.id.edtEntrada)
         edtSalida=findViewById(R.id.edtSalida)
+        tlCelulares=findViewById(R.id.tlCelulares)
+        llenarTabla()
     }
     fun insertar(view:View) {
         var con = SqlDB(this, "Tienda", null, 1)
@@ -127,5 +133,27 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, "El registro no fue encontrado", Toast.LENGTH_LONG).show()
             }
         }
+    }
+    fun llenarTabla(){
+        tlCelulares?.removeAllViews()
+        val con=SqlDB(this,"Tienda", null, 1)
+        val baseDatos=con.writableDatabase
+        val fila=baseDatos.rawQuery("select codigo, marca, modelo, entrada, salida from inventario", null)
+        fila.moveToFirst()
+        do{
+            val registro=LayoutInflater.from(this).inflate(R.layout.item_table_layout_ed, null, false)
+            val tvCodigo=registro.findViewById<View>(R.id.tvCodigo) as TextView
+            val tvMarca=registro.findViewById<View>(R.id.tvMarca) as TextView
+            val tvModelo=registro.findViewById<View>(R.id.tvModelo) as TextView
+            val tvEntrada=registro.findViewById<View>(R.id.tvEntrada) as TextView
+            val tvSalida=registro.findViewById<View>(R.id.tvSalida) as TextView
+            tvCodigo.setText(fila.getString(0))
+            tvMarca.setText(fila.getString(1))
+            tvModelo.setText(fila.getString(2))
+            tvEntrada.setText(fila.getString(3))
+            tvSalida.setText(fila.getString(4))
+            tlCelulares?.addView(registro)
+
+        }while (fila.moveToNext())
     }
 }
