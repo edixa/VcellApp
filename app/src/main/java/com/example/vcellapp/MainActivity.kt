854b09d6bc7f6c99dcb16b5ea74726eac5bfffd8
Ed <75823130+edixa@users.noreply.github.com>
2021-09-05 11:14:01 -0500

    cree la funcion insertar,agregue botones y les di valores

--- app/src/main/java/com/example/vcellapp/MainActivity.kt
+++ app/src/main/java/com/example/vcellapp/MainActivity.kt
@@ -1,11 +1,68 @@
 package com.example.vcellapp
 
+import android.content.ContentValues
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    var edtCodigo:EditText?=null
+    var edtMarca:EditText?=null
+    var edtModelo:EditText?=null
+    var edtCantidad:EditText?=null
+    var edtEntrada:EditText?=null
+    var edtSalida:EditText?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        edtCodigo=findViewById(R.id.edtCodigo)
+        edtMarca=findViewById(R.id.edtMarca)
+        edtModelo=findViewById(R.id.edtModelo)
+        edtCantidad=findViewById(R.id.edtCantidad)
+        edtEntrada=findViewById(R.id.edtEntrada)
+        edtSalida=findViewById(R.id.edtSalida)
+    }
+    fun insertar(view:View) {
+        var con = SqlDB(this, "Tienda", null, 1)
+        var baseDatos = con.writableDatabase
+
+        var codigo = edtCodigo?.text.toString()
+        var marca = edtMarca?.text.toString()
+        var modelo = edtModelo?.text.toString()
+        var cantidad = edtCantidad?.text.toString()
+        var entrada = edtEntrada?.text.toString()
+        var salida = edtSalida?.text.toString()
+
+        if (codigo.isEmpty() == false && marca.isEmpty() == false && modelo.isEmpty() == false && cantidad.isEmpty() == false && entrada.isEmpty() == false && salida.isEmpty() == false) {
+            var inventario=ContentValues()
+            inventario.put("codigo", codigo)
+            inventario.put("marca", marca)
+            inventario.put("modelo", modelo)
+            inventario.put("cantidad", cantidad)
+            inventario.put("entrada", entrada)
+            inventario.put("salida", salida)
+            baseDatos.insert("inventario", null, inventario)
+            edtCodigo?.setText("")
+            edtMarca?.setText("")
+            edtModelo?.setText("")
+            edtCantidad?.setText("")
+            edtEntrada?.setText("")
+            edtSalida?.setText("")
+            Toast.makeText(this,"Se ha ingresado exitosamente", Toast.LENGTH_LONG).show()
+        } else{
+            Toast.makeText(this,"Los campos deden ser llenados", Toast.LENGTH_LONG).show()
+        }
+    }
+    fun buscar(view:View){
+        val con=SqlDB(this, "Tienda", null, 1)
+        val baseDatos=con.writableDatabase
+        val codigo=edtCodigo?.text.toString()
+        if(codigo.isEmpty()==false){
+            val fila=baseDatos.rawQuery("select from inventario")
+        }
     }
 }
---
+++ app/src/main/java/com/example/vcellapp/SqlDB.kt
@@ -0,0 +1,19 @@
+package com.example.vcellapp
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class SqlDB(
+    context: Context?,
+    name: String?,
+    factory: SQLiteDatabase.CursorFactory?,
+    version: Int
+) : SQLiteOpenHelper(context, name, factory, version) {
+    override fun onCreate(db: SQLiteDatabase?) {
+      db?.execSQL("create table inventario (codigo int primary key, marca text, modelo text, cantidad int, entrada int, salida int)")
+    }
+
+    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
+        TODO("Not yet implemented")
+    }
+}
